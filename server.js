dfba447dd73ce9811e0fc58d76713a3084591564
shavyshev <leo.panchevre@gmail.com>
2018-09-18 16:21:22 +0200

    TP1 de POOC

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+    response.send('<h1>Bonjour je suis Léo</h1>')
+})
+
+app.get('/hello/:name', (request, response) => {
+    response.send('Hello, ' + request.params.name + '!');
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('Erreur du serveur : ', err)
+    }
+
+    console.log('Le serveur écoute sur le port' + port + 'nRendez vous sur http://localhost:' + port)})
+
+
+
+
+
